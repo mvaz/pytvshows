e0c7f2a6ba48c5773a8ebdc81507c43de0e85b56
Ben Firshman <ben@firshman.co.uk>
2007-11-28 18:45:25 +0000

    * Moved to setuptools * Added pytvshows library

--- setup.py
+++ setup.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import ez_setup
+# install fails completely if setuptools is installed but isn't latest 
+# without version specified here.
+ez_setup.use_setuptools(version="0.6c1")
+
+from setuptools import setup, find_packages
 
 def normalizeWhitespace(s):
     return ' '.join(s.split())
 
-setup(name='pytvshows',
-      version='0.2+svn',
-      description='A simple, shell based tvrss.net torrent downloader.',
-      author='Ben Firshman',
-      author_email='ben@firshman.co.uk',
-      url='http://pytvshows.sourceforge.net/',
-      long_description=normalizeWhitespace("""pytvshows downloads torrents for 
-      TV shows from RSS feeds provided by tvRSS.net. It is designed to be run 
-      from cron or a shell for programs like rTorrent. It is based on TVShows 
-      (http://tvshows.sourceforge.net/)."""),
-      scripts=['pytvshows',]
-     )
+setup(
+    name = 'pytvshows',
+    version = '0.2+svn',
+    description = 'A simple, shell based tvrss.net torrent downloader.',
+    author = 'Ben Firshman',
+    author_email = 'ben@firshman.co.uk',
+    url = 'http://pytvshows.sourceforge.net/',
+    long_description = normalizeWhitespace("""pytvshows downloads torrents for 
+    TV shows from RSS feeds provided by tvRSS.net. It is designed to be run 
+    from cron or a shell for programs like rTorrent. It is based on TVShows 
+    (http://tvshows.sourceforge.net/)."""),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications",
+        "Topic :: Internet",
+        "Topic :: Other/Nonlisted Topic",
+        "Environment :: Console",
+        ],
+    
+    scripts = ['pytvshows',],
+    py_modules = ['pytvshows',],
+    
+    install_requires = [
+        'FeedParser>=4.0',
+        #'BitTorrent-bencode>=5.0.8',
+        ],
+    )
