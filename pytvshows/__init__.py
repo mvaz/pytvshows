9461c1124d4dbc63eec33da0d02802b032b8d2ea
Ben Firshman <ben@firshman.co.uk>
2008-12-24 00:59:09 +0000

    Switched to a BSD license

--- pytvshows/__init__.py
+++ pytvshows/__init__.py
@@ -1,28 +1,12 @@
 # encoding: utf-8
 """
-PyTVShows - Library
+PyTVShows
 http://pytvshows.sourceforge.net/
-
-Copyright (C) 2007, Ben Firshman
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 __version__ = '0.2+svn'
 
-USER_AGENT = "PyTVShows/%s +http://pytvshows.sourceforge.net/" % __version__
+USER_AGENT = "PyTVShows/%s" % __version__
 
 import sys
 if not hasattr(sys, "version_info") or sys.version_info < (2,4):
